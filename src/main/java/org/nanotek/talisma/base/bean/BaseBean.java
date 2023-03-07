06a7a8169e5af7247549dd4bc1c45a4322366fb6
José Carlos Canova <jose.canova@actdigital.com>
2023-03-07 09:58:29 -0300

    Model Update

---
+++ src/main/java/org/nanotek/talisma/base/bean/BaseBean.java
@@ -0,0 +1,9 @@
+package org.nanotek.talisma.base.bean;
+
+import org.nanotek.talisma.base.Base;
+
+public abstract class BaseBean<T> implements Base<T>{
+
+	private static final long serialVersionUID = 7712002466802976928L;
+
+}
